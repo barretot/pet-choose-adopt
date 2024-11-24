511182288613efd82955a369d3f02d5e7ba76486
barretot <barretoruan77@gmail.com>
2024-11-23 23:32:29 -0300

    data builder tests pattern finished

--- test/builders/user-builder.ts
+++ test/builders/user-builder.ts
@@ -3,45 +3,42 @@ import { CryptographyAdapterMock } from 'test/mocks/cryptography/cryptography-ad
 import { InMemoryDatabaseService } from 'test/mocks/repositories/in-memory-database.service'
 import { InMemoryUserRepository } from 'test/mocks/repositories/in-memory-user-repository'
 
+interface ValidUser {
+  name: string
+  email: string
+  password: string
+  id: string
+}
+
+interface MockDependencies {
+  hasher: CryptographyAdapterMock
+  inMemoryUserRepository: InMemoryUserRepository
+  validUser: ValidUser
+}
+
 export class UserBuilder {
-  private mockDependencies: {
-    hasher: CryptographyAdapterMock
-    inMemoryDatabaseService: InMemoryDatabaseService<User>
-    inMemoryUserRepository: InMemoryUserRepository
-  }
+  private mockDependencies: MockDependencies
 
   constructor() {
-    const inMemoryDatabaseService = new InMemoryDatabaseService<User>()
-    const inMemoryUserRepository = new InMemoryUserRepository(
-      inMemoryDatabaseService,
-    )
-    const hasher = new CryptographyAdapterMock()
-
     this.mockDependencies = {
-      hasher,
-      inMemoryDatabaseService,
-      inMemoryUserRepository,
+      hasher: new CryptographyAdapterMock(),
+      inMemoryUserRepository: new InMemoryUserRepository(
+        new InMemoryDatabaseService<User>(),
+      ),
+      validUser: {
+        name: 'Jane Doe',
+        email: 'jane.doe@mail.com',
+        password: 'plaintextPassword',
+        id: 'custom-id',
+      },
     }
   }
 
   public success(): UserBuilder {
-    // Configurações específicas para um cenário "de sucesso"
     return this
   }
 
-  public withCustomDependency(
-    dependencyName: string,
-    instance: unknown,
-  ): UserBuilder {
-    this.mockDependencies[dependencyName] = instance
-    return this
-  }
-
-  public build(): {
-    hasher: CryptographyAdapterMock
-    inMemoryDatabaseService: InMemoryDatabaseService<User>
-    inMemoryUserRepository: InMemoryUserRepository
-  } {
+  public build(): MockDependencies {
     return this.mockDependencies
   }
 }
--- test/use-cases/create-user.use-case.spec.ts
+++ test/use-cases/create-user.use-case.spec.ts
@@ -1,21 +1,13 @@
-import { User } from '@/core/domain/entities/user/User'
 import { CreateUserUseCase } from '@/use-cases/user/create-user.use-case'
 import { UserBuilder } from 'test/builders/user-builder'
 
 describe('Create user Use Case', () => {
   it('should return success when creating a user', async () => {
-    const { inMemoryUserRepository, hasher } = new UserBuilder()
+    const { inMemoryUserRepository, hasher, validUser } = new UserBuilder()
       .success()
       .build()
 
     const sut = new CreateUserUseCase(inMemoryUserRepository, hasher)
-
-    const validUser: User = {
-      name: 'Jane Doe',
-      email: 'jane.doe@mail.com',
-      password: 'plaintextPassword',
-      id: 'custom-id',
-    }
 
     const createUserResponse = await sut.execute(validUser)
 
